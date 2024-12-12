590eb3adcdc878de9952dc53bb92f1249636fae9
Mahdis Amin <mahdis.amin95@gmail.com>
2024-12-13 01:06:34 +0330

    some change

--- torino/src/app/user/page.js
+++ torino/src/app/user/page.js
@@ -25,37 +25,37 @@ function UserInfo() {
     console.log("hi");
   };
   return (
-    <div className={styles.container}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className={styles.input}>
-          <div>
-            <h2>
-              <FaUser style={{ fontSize: "15px", padding: "0 10px" }} />
-              مشخصات مسافر
-            </h2>
+    <Suspense>
+      <div className={styles.container}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.input}>
+            <div>
+              <h2>
+                <FaUser style={{ fontSize: "15px", padding: "0 10px" }} />
+                مشخصات مسافر
+              </h2>
+            </div>
+            <input
+              placeholder="نام و نام خانوادگی"
+              {...register("name", { required: true })}
+            />
+            {errors.name && <span>این فیلد الزامی است </span>}
+            <input
+              placeholder="کد ملی"
+              {...register("name", { required: true })}
+            />
+            {errors.name && <span>این فیلد الزامی است </span>}{" "}
+            <input
+              placeholder="تاریخ تولد"
+              {...register("name", { required: true })}
+            />
+            {errors.name && <span>این فیلد الزامی است </span>}{" "}
+            <input
+              placeholder="جنسیت"
+              {...register("name", { required: true })}
+            />
+            {errors.name && <span> این فیلد الزامی است </span>}
           </div>
-          <input
-            placeholder="نام و نام خانوادگی"
-            {...register("name", { required: true })}
-          />
-          {errors.name && <span>این فیلد الزامی است </span>}
-          <input
-            placeholder="کد ملی"
-            {...register("name", { required: true })}
-          />
-          {errors.name && <span>این فیلد الزامی است </span>}{" "}
-          <input
-            placeholder="تاریخ تولد"
-            {...register("name", { required: true })}
-          />
-          {errors.name && <span>این فیلد الزامی است </span>}{" "}
-          <input
-            placeholder="جنسیت"
-            {...register("name", { required: true })}
-          />
-          {errors.name && <span> این فیلد الزامی است </span>}
-        </div>
-        <Suspense>
           <div className={styles.tourCard}>
             <div className={styles.tourTitle}>
               <h1>{title}</h1>
@@ -70,9 +70,9 @@ function UserInfo() {
             </div>
             <button type="submit"> ثبت و خرید نهایی </button>
           </div>
-        </Suspense>
-      </form>
-    </div>
+        </form>
+      </div>
+    </Suspense>
   );
 }
 
