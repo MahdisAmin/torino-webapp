48a440c50e2cc68eecd6ea3be807d4f92384c9c3
Mahdis Amin <mahdis.amin95@gmail.com>
2024-12-19 03:01:24 +0330

    dropdown menu mostly developed

---
+++ torino/src/components/element/Dropdown.js
@@ -0,0 +1,58 @@
+import { toPersianNumber } from "@/utils/extras";
+import styles from "./Dropdown.module.css";
+import { FaUser } from "react-icons/fa6";
+import { PiSignOutLight } from "react-icons/pi";
+import { CiUser } from "react-icons/ci";
+import Link from "next/link";
+
+function Dropdown({ show, phone }) {
+  if (!show) return;
+  return (
+    <div className={styles.container}>
+      <div className={styles.phone}>
+        <div className={styles.first}>
+          <FaUser
+            style={{
+              fontSize: "15px",
+              color: "#696969",
+              marginBottom: "-5px",
+            }}
+          />
+        </div>
+        <div className={styles.second}>
+          <h4>{toPersianNumber(phone)}</h4>
+        </div>
+      </div>
+      <div className={styles.user}>
+        <div className={styles.firstuser}>
+          <CiUser
+            style={{
+              fontSize: "20px",
+              color: "#696969",
+              marginBottom: "-5px",
+            }}
+          />
+        </div>
+        <div className={styles.seconduser}>
+          <Link href="/profile">اطلاعات حساب کاربری</Link>
+        </div>
+      </div>
+      <div className={styles.out}>
+        <div className={styles.firstout}>
+          <PiSignOutLight
+            style={{
+              fontSize: "20px",
+              color: "#D40000",
+              marginBottom: "-5px",
+            }}
+          />
+        </div>
+        <div className={styles.secondout}>
+          <h4>خروج از حساب کاربری</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dropdown;
--- torino/src/components/modal/Auth.js
+++ torino/src/components/modal/Auth.js
@@ -5,29 +5,51 @@ import ReactOtpInput from "./CodeModal";
 import { useGetUserData } from "@/core/services/query";
 import Styels from "../../app/layout.module.css";
 import { toPersianNumber } from "@/utils/extras";
+import { FaChevronDown, FaUser } from "react-icons/fa6";
+import Dropdown from "../element/Dropdown";
+
 
 function Auth() {
   const [showModal, setShowModal] = useState(false);
-
-  const enterHandler = () => {
-    setShowModal(true);
-  };
+  const [showDropdown , setShowDropdown]=useState(false)
   const [step, setStep] = useState(1);
   const [mobile, setMobile] = useState("");
   const { isPending, data } = useGetUserData();
+  const phoneNumber = data?.data.mobile;
+  const enterHandler = () => {
+    setShowModal(true);
+  };
 
   const closeModal = () => {
     setShowModal(false);
   };
+  const dropDownHandler = () => {
+   setShowDropdown(!showDropdown)
+    
+  }
 
   if (isPending && !data?.data) return <h4>Loading...</h4>;
   if (data?.data)
     return (
-      <div className={Styels.link}>
-        <select>
-          <option>{toPersianNumber(data?.data.mobile)}</option>
-          <option>حساب کاربری</option>
-        </select>
+      <div className={Styels.link} onClick={dropDownHandler}>
+        <h4>
+          <FaUser
+            style={{
+              marginLeft: "5px",
+              fontSize: "15px",
+              marginBottom: "-2px",
+            }}
+          />
+          {toPersianNumber(phoneNumber)}
+          <FaChevronDown
+            style={{
+              marginBottom: "-2px",
+              marginRight: "5px",
+              fontSize: "15px",
+            }}
+          />
+        </h4>
+        <Dropdown show={showDropdown} phone={phoneNumber} />
       </div>
     );
   return (
@@ -39,6 +61,7 @@ function Auth() {
             ورود | ثبت نام
           </span>
         </button>
+     
       </div>
       {step === 1 && (
         <Signup
