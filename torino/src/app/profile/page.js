44f07de8e0a55a776b515ca7d99d1bf347c8b2ff
Mahdis Amin <mahdis.amin95@gmail.com>
2024-12-22 03:06:04 +0330

    ui of profile page updated

--- torino/src/app/profile/page.js
+++ torino/src/app/profile/page.js
@@ -5,7 +5,8 @@ import { useGetUserData } from "@/core/services/query";
 import { toPersianNumber } from "@/utils/extras";
 
 function Profile({ children }) {
-  const {data} = useGetUserData()
+  const { data } = useGetUserData()
+  if(!data)return
   return (
     <>
       <div className={styles.profileContainer}>
@@ -13,7 +14,7 @@ function Profile({ children }) {
         <div className={styles.userInfo}>
           <div>
             <p>شماره موبایل:</p>
-            <span>{(data?.data.mobile)}</span>
+            <span>{toPersianNumber(data?.data.mobile)}</span>
           </div>
           <div className={styles.emailInfo}>
             <p>ایمیل:</p>
@@ -24,36 +25,52 @@ function Profile({ children }) {
           </div>
         </div>
       </div>
-      <div className={styles.profileContainer}>
-        <h3>اطلاعات حساب کاربری</h3>
+      <div className={styles.personProfileContainer}>
+        <div className={styles.personInfo}>
+          <h3>اطلاعات شخصی</h3>
+          <p>
+            <BiEditAlt />
+            ویرایش اطلاعات
+          </p>
+        </div>
         <div className={styles.userInfo}>
           <div>
-            <p>شماره موبایل:</p>
+            <p>نام و نام خانوادگی</p>
           </div>
           <div className={styles.emailInfo}>
-            <p>ایمیل:</p>
-            <p>
-              <BiEditAlt />
-              افزودن
-            </p>
+            <p style={{ color: "#282828" }}>کد ملی</p>
+          </div>
+        </div>
+        <div className={styles.userInfo}>
+          <div>
+            <p>جنسیت</p>
+          </div>
+          <div className={styles.emailInfo}>
+            <p style={{ color: "#282828" }}>تاریخ تولد</p>
           </div>
         </div>
       </div>
-      <div className={styles.profileContainer}>
-        <h3>اطلاعات حساب کاربری</h3>
+      <div className={styles.personProfileContainer}>
+        <div className={styles.personInfo}>
+          <h3>اطلاعات حساب بانکی</h3>
+          <p>
+            <BiEditAlt />
+            ویرایش اطلاعات
+          </p>
+        </div>
         <div className={styles.userInfo}>
           <div>
-            <p>شماره موبایل:</p>
+            <p>شماره شبا</p>
           </div>
           <div className={styles.emailInfo}>
-            <p>ایمیل:</p>
-            <p>
-              <BiEditAlt />
-              افزودن
-            </p>
+            <p style={{ color: "#282828" }}>شماره کارت</p>
           </div>
         </div>
-        <main>{children}</main>
+        <div className={styles.userInfo}>
+          <div>
+            <p>شماره حساب</p>
+          </div>
+        </div>
       </div>
     </>
   );
